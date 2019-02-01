e54a8c83a6cb46d15704c345d5ac03e9876a3dc3
PMatrixMedia <33465098+PMatrixMedia@users.noreply.github.com>
2019-01-31 22:40:16 -0600

    Component
    Separated Directory items into a different component within the app.

--- src/app/artist-items/artist-items.component.ts
+++ src/app/artist-items/artist-items.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-artist-items',
-  templateUrl: './artist-items.component.html',
-  styleUrls: ['./artist-items.component.css']
+  selector: "app-artist-items",
+  templateUrl: "./artist-items.component.html",
+  styleUrls: ["./artist-items.component.css"],
+  inputs: ["artist"]
 })
 export class ArtistItemsComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
